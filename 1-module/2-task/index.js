735bf4c0ed1dbe0a56d88e9c394abc0288eebe21
Fedor <dr.blackshaper@yandex.ru>
2024-03-28 17:18:09 +0300

    added fuction isValid

--- 1-module/2-task/index.js
+++ 1-module/2-task/index.js
@@ -10,15 +10,29 @@ function print(text) {
  * чтобы функция sayHello работала корректно
  */
 function isValid(name) {
-  // ваш код...
+  if (name == null) {
+    return false;
+  }
+  let lenght = 0;
+  for (i of name) {
+    lenght += 1;
+    if (i == " ") {
+      return false;
+    }
+  }
+  if (lenght <= 3) {
+    return false;
+  }
+
+  return true;
 }
 
 function sayHello() {
-  let userName = prompt('Введите ваше имя');
+  let userName = prompt("Введите ваше имя");
 
   if (isValid(userName)) {
     print(`Welcome back, ${userName}!`);
   } else {
-    print('Некорректное имя');
+    print("Некорректное имя");
   }
 }
